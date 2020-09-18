2114b6a3cc2a4834a28e4c9165d4502c051e296a
MWLeonel <wmleonel@gmail.com>
2020-09-18 10:01:26 -0300

    Fahrenheit para Celsius

---
+++ GrausFC/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace GrausFC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double Fahrenheit;
+            double Celsius = 97;
+
+            Console.Write("Digite seu Valor Fahrenheit: ");
+            Fahrenheit = double.Parse(Console.ReadLine());
+
+            Celsius = (Fahrenheit - 32) / 1.8;
+            Console.WriteLine("Fahrenheit para Celsius = {0}", Celsius);
+            Console.Write("\n");
+
+            Console.WriteLine("Pressione ENTER para sair!");
+            Console.ReadKey();
+        }
+    }
+}
